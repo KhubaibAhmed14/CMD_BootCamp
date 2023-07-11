a07a2c7687817cb93067de4a744fafa5aaa8190c
KhubaibAhmed14 <59171567+KhubaibAhmed14@users.noreply.github.com>
2023-07-11 00:49:42 -0400

    Add files via upload

---
+++ 00-CMD BootCamp/A2T1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    internal class A2T1
+
+    {
+        class Animal
+        {
+            string name;
+            string species;
+            int age;
+
+            public Animal(string name, int age, string species) {
+                this.name = name;
+                this.species = species;
+                this.age = age;
+            }
+
+            public virtual void eat() {
+                Console.WriteLine("{0} is eating.", name);
+            }
+            public string getName() { return name; }
+            public int getAge() { return age; }
+            public string getSpecies() { return species;}
+        }
+
+        class Dog : Animal
+        {
+            public Dog(string name, int age, string species) : base(name, age, species)
+            {
+            }
+
+            public override void eat()
+            {
+                Console.WriteLine("The {0} is licking bone.", getName());
+            }
+
+            public void Bark()
+            {
+                Console.WriteLine("Dog is barking.");
+            }
+        }
+        class Lion : Animal
+        {
+            public Lion(string name, int age, string species) : base(name, age, species)
+            {
+            }
+            public override void eat()
+            {
+                Console.WriteLine("The {0} is eating deer.", getName());
+            }
+            public void Roar()
+            {
+                Console.WriteLine("Lion is roaring.");
+            }
+        }
+        class Elephant : Animal
+        {
+            public Elephant(string name, int age, string species) : base(name, age, species)
+            {
+            }
+            public override void eat()
+            {
+                Console.WriteLine("The {0} is eating peanuts.", getName());
+            }
+            public void Trumpet()
+            {
+                Console.WriteLine("Elephant Trumpets.");
+            }
+        }
+        class Zoo
+        {
+            List<Animal> animals;
+
+            public Zoo(List<Animal> animals)
+            {
+                this.animals = animals;
+            }
+
+            public Zoo()
+            {
+                this.animals = new List<Animal>();
+            }
+
+            public void AddAnimal(Animal animal)
+            {
+                animals.Add(animal);
+            }
+
+            public void FeedAllAnimal()
+            {
+                foreach (Animal animal in animals)
+                {
+                    animal.eat();
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Zoo myZoo = new Zoo();
+            Dog myDog = new Dog("Scooby", 3, "abc");
+            Elephant myElephant = new Elephant("Hathi", 3, "abc");
+            Lion myLion = new Lion("Mufasa", 3, "abc");
+
+            myDog.Bark();
+            myElephant.Trumpet();
+            myLion.Roar();
+
+            myZoo.AddAnimal(myDog);
+            myZoo.AddAnimal(myElephant);
+            myZoo.AddAnimal(myLion);
+            myZoo.FeedAllAnimal();
+        }
+    }
+}
---
+++ 00-CMD BootCamp/A2T2.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace A2_T2
+{
+    internal class A2T2
+    {
+
+        class BankAccount
+            {
+            int AccountNumber;
+            string AccountHolderName;
+            decimal Balance;
+
+            public BankAccount(int accountNumber, string accountHolderName, decimal balance)
+            {
+                AccountNumber = accountNumber;
+                AccountHolderName = accountHolderName;
+                Balance = balance;
+            }
+            public BankAccount() { }
+
+            public decimal getBalance() { return Balance; }
+            public int getAccountNumber() { return AccountNumber; }
+            public string getAccountHolder() { return AccountHolderName; }
+            public void setBalance(decimal balance) { Balance = balance; }
+            public void setAccountNumber(int accountNumber) { AccountNumber = accountNumber; }
+            public void setAccountHolderName(string accountHolderName) { AccountHolderName=accountHolderName; }
+
+            public virtual void Deposit(decimal amount)
+            {
+                Balance += amount;
+            }
+
+            public virtual void Withdraw(decimal amount)
+            {
+                Balance -= amount;
+            }
+
+            public void DisplayAccountInfo()
+            {
+                Console.WriteLine($"Account Number: {AccountNumber}");
+                Console.WriteLine($"Account Holder: {AccountHolderName}");
+                Console.WriteLine($"Balance: {Balance:C}");
+            }
+        }
+
+        class SavingsAccount : BankAccount
+        {
+            public decimal InterestRate { get; set; }
+
+            public SavingsAccount(int accountNumber, string accountHolderName, decimal balance, decimal interestRate) :
+                base(accountNumber, accountHolderName, balance)
+            { 
+                InterestRate = interestRate;
+            }
+
+            public override void Deposit(decimal amount)
+            {
+                amount = amount * InterestRate;
+                base.Deposit(amount);
+            }
+        }
+
+        class CheckingAccount : BankAccount
+        {
+            public override void Withdraw(decimal amount)
+            {
+                if (getBalance() <= amount)
+                {
+                    base.Withdraw(amount);
+                }
+                else Console.WriteLine("Insufficient Balance.");
+            }
+        }
+
+        class Bank
+        {
+            List<BankAccount> accounts;
+            public Bank()
+            {
+                this.accounts = new List<BankAccount>();
+            }
+            public Bank(List<BankAccount> accounts)
+            {
+                this.accounts = accounts;
+            }
+
+            public void AddAccount(BankAccount account)
+            {
+                this.accounts.Add(account);
+            }
+            public void DepositToAccount(BankAccount account, decimal amount)
+            {
+                bool state = false;
+                foreach(BankAccount account2 in this.accounts)
+                {
+                    if(account2 == account) account.Deposit(amount);
+                    state = true;
+                }
+                if (state) { Console.WriteLine($"{account.getAccountNumber()} not found in the Bank."); }
+            }
+
+            public void WithdrawFronAccount(BankAccount account, decimal amount)
+            {
+                bool state = false;
+                foreach (BankAccount account2 in this.accounts)
+                {
+                    if (account2 == account) account.Withdraw(amount);
+                    state = true;
+                }
+                if (state) { Console.WriteLine($"{account.getAccountNumber()} not found in the Bank."); }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            SavingsAccount savingsAccount = new SavingsAccount(123, "Ali", (decimal)50000.0, (decimal)0.5);
+
+        }
+    }
+}
